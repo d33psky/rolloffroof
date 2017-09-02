3544ddf15be4cb42207b7611088f777557da5a00
d33psky <d33psky@users.noreply.github.com>
2017-09-02 20:27:01 +0000

    add overlay text

--- hazcam2/hazcam.sh
+++ hazcam2/hazcam.sh
@@ -13,7 +13,7 @@ AVERAGEVALUE=0
 SHUTTERSPEED=2
 SHUTTERSPEED=100000
 SHUTTERSPEED=6000000
-SHUTTERSPEED=1000000
+SHUTTERSPEED=4000000
 
 while :; do 
 	NOW=$(date '+%s')
@@ -24,49 +24,74 @@ while :; do
 
 	raspistill -v -n -ex verylong -mm spot -t 1000 -ss $SHUTTERSPEED -q 100 -ISO 800 -o ${T}-rpi2.jpg
 
-	echo "cp ${T}-rpi2.jpg /mnt/live/hazcam2.jpg.tmp"
-	cp ${T}-rpi2.jpg /mnt/live/hazcam2.jpg.tmp
-	echo "mv /mnt/live/hazcam2.jpg.tmp /mnt/live/hazcam2.jpg"
-	mv /mnt/live/hazcam2.jpg.tmp /mnt/live/hazcam2.jpg
-
-#	AVERAGEVALUE=$(convert ${T}-rpi2.jpg -resize 1x1 txt: |tail -1|sed -e's/0,0: (//' -e's/,/ /g'|awk '{ printf "%.0f\n", ($1+$2+$3)/3 }')
+	echo "Calculate average"
 	AVERAGEVALUE=$(cat ${T}-rpi2.jpg|convert -crop 1750x1944 - - |convert -resize 1x1 - txt: |tail -1|sed -e's/0,0: (//' -e's/,/ /g'|awk '{ printf "%.0f\n", ($1+$2+$3)/3 }')
 	echo "AVERAGEVALUE=$AVERAGEVALUE"
 
+	OLDSHUTTERSPEED=$SHUTTERSPEED
 	echo "SHUTTERSPEED=$SHUTTERSPEED , adjust to :"
-	if [[ $AVERAGEVALUE -lt 80 ]]; then
-		if [[ $SHUTTERSPEED -gt 10000 ]]; then
-			SHUTTERSPEED=$((SHUTTERSPEED + 500000))
-		else
-			SHUTTERSPEED=$((SHUTTERSPEED + 100))
-		fi
+ 	if [[ $AVERAGEVALUE -le 10 ]]; then
+ 		SHUTTERSPEED=6000000
+		MUSTGOFAST=1
+ 	elif [[ $AVERAGEVALUE -lt 90 ]]; then
+		MUSTGOFAST=0
+ 		if [[ $SHUTTERSPEED -gt 10000 ]]; then
+ 			SHUTTERSPEED=$((SHUTTERSPEED + 1000000))
+ 		else
+ 			SHUTTERSPEED=$((SHUTTERSPEED + 100))
+ 		fi
+ 	elif [[ $AVERAGEVALUE -ge 250 ]]; then
+ 		SHUTTERSPEED=1
+		MUSTGOFAST=1
+ 	elif [[ $AVERAGEVALUE -gt 110 ]]; then
+		MUSTGOFAST=0
+ 		if [[ $SHUTTERSPEED -gt 10000 ]]; then
+ 			SHUTTERSPEED=$((SHUTTERSPEED - 1000000))
+ 		else
+ 			SHUTTERSPEED=$((SHUTTERSPEED - 100))
+ 		fi
+ 	fi
 
-		if [[ $SHUTTERSPEED -gt 6000000 ]]; then
-			SHUTTERSPEED=6000000
-		fi
-	elif [[ $AVERAGEVALUE -gt 120 ]]; then
-		if [[ $SHUTTERSPEED -gt 10000 ]]; then
-			SHUTTERSPEED=$((SHUTTERSPEED - 500000))
-		else
-			SHUTTERSPEED=$((SHUTTERSPEED - 100))
-		fi
-		if [[ $SHUTTERSPEED -lt 1 ]]; then
-			SHUTTERSPEED=1
-		fi
+	if [[ $SHUTTERSPEED -gt 6000000 ]]; then
+		echo "SHUTTERSPEED=$SHUTTERSPEED too large, set to max 6000000"
+		SHUTTERSPEED=6000000
+	elif [[ $SHUTTERSPEED -lt 1 ]]; then
+		echo "SHUTTERSPEED=$SHUTTERSPEED too small, set to min 1"
+		SHUTTERSPEED=1
+	else
+		echo "SHUTTERSPEED=$SHUTTERSPEED"
 	fi
-	echo "SHUTTERSPEED=$SHUTTERSPEED"
+
+	echo "cp ${T}-rpi2.jpg /mnt/live/hazcam2_temp2.jpg"
+	cp ${T}-rpi2.jpg /mnt/live/hazcam2_temp2.jpg
 
     NOW=$(date '+%s')
     DIFF=$((NOW - START))
     if [[ $DIFF -ge $TARGETPERIOD ]]; then
 		LATE=$((DIFF - TARGETPERIOD))
         echo "We're $LATE s late to meet the $TARGETPERIOD s period, sleep 10 anyway"
-		sleep 10
+		SLEEP=10
     else
         MUSTSLEEP=$((TARGETPERIOD - DIFF))
         echo "Sleep $MUSTSLEEP s to match the $TARGETPERIOD s period"
-        sleep $MUSTSLEEP
+		SLEEP=$MUSTSLEEP
     fi
+
+	OLDEXPOSURE=$(/bin/echo "scale=6; $OLDSHUTTERSPEED/1000000" | /usr/bin/bc -l)
+	NEWEXPOSURE=$(/bin/echo "scale=6; $SHUTTERSPEED/1000000" | /usr/bin/bc -l)
+	LINE="hazcam2 rpi2\n$(date -u '+%Y-%m-%dT%H:%M:%SZ')\nAverage pixel value = $AVERAGEVALUE\nExposure $OLDEXPOSURE s\n (next $NEWEXPOSURE)\nISO=$ISO\n (next $ISO)\nSleep $SLEEP s"
+	echo "convert /mnt/live/hazcam2_temp2.jpg -fill white -pointsize 60 -annotate +50+90 "$LINE" /mnt/live/hazcam2_temp3.jpg"
+	convert /mnt/live/hazcam2_temp2.jpg -fill white -pointsize 60 -annotate +50+90 "$LINE" /mnt/live/hazcam2_temp3.jpg
+
+	echo "mv /mnt/live/hazcam2_temp3.jpg /mnt/live/hazcam2.jpg"
+	mv /mnt/live/hazcam2_temp3.jpg /mnt/live/hazcam2.jpg
+
+	if [[ $MUSTGOFAST -eq 1 ]]; then
+		continue
+	fi
+
+	echo "sleep $SLEEP"
+	sleep $SLEEP
 
 done
 
