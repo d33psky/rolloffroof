64a4c8b654f3b40d3cc56c8a5b4b957b3df4e391
d33psky <d33psky@users.noreply.github.com>
2017-10-18 12:59:07 +0000

    refactor allsky into hazcam

--- hazcam2/hazcam.sh
+++ hazcam2/hazcam.sh
@@ -3,59 +3,59 @@
 # rpi2 hazcam2
 
 set -e
-#set -x
 
-cd /mnt/nas/rpi2/tmp/
+CAMNAME="hazcam2"
+CAMHOST="rpi2"
+
+COMMAND="cd /mnt/nas/${CAMHOST}/tmp/"
+T=$(date '+%Y%m%d_%H%M%S') ; echo "$T $COMMAND" ; $COMMAND
+LIVEDIR="/mnt/live"
 
 TARGETPERIOD=60
-
 AVERAGEVALUE=0
-SHUTTERSPEED=2
-SHUTTERSPEED=6000000
-SHUTTERSPEED=4000000
-SHUTTERSPEED=100000
+TARGETVALUE=128
 
-ISO=800
+SHUTTERSPEED=400000
+SHUTTERSPEED=1
+SHUTTERSPEED=100
+SHUTTERSPEED=20
+SHUTTERSPEED=10000
+SHUTTERSPEED=6000000
+SHUTTERSPEED=1000000
 
 while :; do 
 	NOW=$(date '+%s')
 	START=$NOW
 
 	T=$(date '+%Y%m%d_%H%M%S')
-	echo "$T shutterspeed = $SHUTTERSPEED"
+	FILENAMEDATE=$T
+	ISO=800
+	echo "$T shutterspeed = $SHUTTERSPEED ISO = $ISO"
 
-	raspistill -v -n -ex verylong -mm spot -t 1000 -ss $SHUTTERSPEED -q 100 -ISO $ISO -o ${T}-rpi2.jpg
+    COMMAND="raspistill -v -n -ex verylong -mm spot -t 1000 -ss $SHUTTERSPEED -q 100 -ISO $ISO -o ${FILENAMEDATE}-${CAMHOST}.jpg"
+    T=$(date '+%Y%m%d_%H%M%S') ; echo "$T $COMMAND" ; $COMMAND
+	# 2592x1944
 
-	echo "Calculate average"
-    # ImageMagick pixel enumeration: 1,1,255,srgb
-    #AVERAGEVALUE=$(cat ${T}-rpi2.jpg|convert -crop 1750x1944 - - |convert -resize 1x1 - txt: |tail -1|sed -e's/0,0: (//' -e's/,/ /g'|awk '{ printf "%.0f\n", ($1+$2+$3)/3 }')
+#	T=$(date '+%Y%m%d_%H%M%S')
+#	echo "$T composite ${LIVEDIR}/${CAMNAME}-mask.png ${FILENAMEDATE}-${CAMHOST}.jpg  ${LIVEDIR}/${CAMNAME}_temp2.jpg"
+#	composite ${LIVEDIR}/${CAMNAME}-mask.png ${FILENAMEDATE}-${CAMHOST}.jpg ${LIVEDIR}/${CAMNAME}_temp2.jpg
+
+	T=$(date '+%Y%m%d_%H%M%S')
+	echo "$T Calculate average"
     # ImageMagick pixel enumeration: 1,1,65535,srgb
-	AVERAGEVALUE=$(cat ${T}-rpi2.jpg|convert -crop 1750x1944 - - |convert -resize 1x1 - txt: |tail -1|sed -e's/0,0: (//' -e's/,/ /g'|awk '{ printf "%.0f\n", 256*(($1+$2+$3)/3)/65535 }')
-	echo "AVERAGEVALUE=$AVERAGEVALUE"
-
+	AVERAGERGB=$(cat ${FILENAMEDATE}-${CAMHOST}.jpg|convert -crop 1000x1000+796+472 - - |convert -resize 1x1 - txt: |tail -1|sed -e's/0,0: (//' -e's/,/ /g'|awk '{ printf "%.0f %.0f %.0f\n", $1/256,$2/256,$3/256 }')
+	RED=$(  echo $AVERAGERGB|cut -d' ' -f 1)
+	GREEN=$(echo $AVERAGERGB|cut -d' ' -f 2)
+	BLUE=$( echo $AVERAGERGB|cut -d' ' -f 3)
+	AVERAGEVALUE=$(echo $AVERAGERGB|awk '{ printf "%.0f\n", ($1+$2+$3)/3 }')
+	T=$(date '+%Y%m%d_%H%M%S')
+	echo -n "$T RGB = $RED $GREEN $BLUE AVERAGE=$AVERAGEVALUE "
 	OLDSHUTTERSPEED=$SHUTTERSPEED
-	echo "SHUTTERSPEED=$SHUTTERSPEED , adjust to :"
- 	if [[ $AVERAGEVALUE -le 10 ]]; then
- 		SHUTTERSPEED=6000000
-		MUSTGOFAST=1
- 	elif [[ $AVERAGEVALUE -lt 90 ]]; then
-		MUSTGOFAST=0
- 		if [[ $SHUTTERSPEED -gt 10000 ]]; then
- 			SHUTTERSPEED=$((SHUTTERSPEED + 1000000))
- 		else
- 			SHUTTERSPEED=$((SHUTTERSPEED + 100))
- 		fi
- 	elif [[ $AVERAGEVALUE -ge 250 ]]; then
- 		SHUTTERSPEED=1
-		MUSTGOFAST=1
- 	elif [[ $AVERAGEVALUE -gt 110 ]]; then
-		MUSTGOFAST=0
- 		if [[ $SHUTTERSPEED -gt 10000 ]]; then
- 			SHUTTERSPEED=$((SHUTTERSPEED - 1000000))
- 		else
- 			SHUTTERSPEED=$((SHUTTERSPEED - 100))
- 		fi
- 	fi
+	DELTAAVERAGEVALUE=$((TARGETVALUE - AVERAGEVALUE)) # range: 128-1=127, or 128-255=-127
+	DELTAFACTOR=$((1 + (OLDSHUTTERSPEED/200))) # range 1..3001
+	SHUTTERSPEED=$((SHUTTERSPEED + (DELTAFACTOR * DELTAAVERAGEVALUE)))
+	echo "DELTAAVERAGE=$DELTAAVERAGEVALUE DELTAFACTOR=$DELTAFACTOR SHUTTERSPEED=${OLDSHUTTERSPEED}->${SHUTTERSPEED}"
+#	AVERAGEVALUE=$(cat ${T}-${CAMHOST}.jpg|convert -crop 1750x1944 - - |convert -resize 1x1 - txt: |tail -1|sed -e's/0,0: (//' -e's/,/ /g'|awk '{ printf "%.0f\n", 256*(($1+$2+$3)/3)/65535 }')
 
 	if [[ $SHUTTERSPEED -gt 6000000 ]]; then
 		echo "SHUTTERSPEED=$SHUTTERSPEED too large, set to max 6000000"
@@ -63,41 +63,61 @@ while :; do
 	elif [[ $SHUTTERSPEED -lt 1 ]]; then
 		echo "SHUTTERSPEED=$SHUTTERSPEED too small, set to min 1"
 		SHUTTERSPEED=1
-	else
-		echo "SHUTTERSPEED=$SHUTTERSPEED"
 	fi
 
-	echo "cp ${T}-rpi2.jpg /mnt/live/hazcam2_temp2.jpg"
-	cp ${T}-rpi2.jpg /mnt/live/hazcam2_temp2.jpg
+#	if [[ $OLDSHUTTERSPEED -eq 6000000 && $AVERAGEVALUE -lt 20 ]]; then
+#		T=$(date '+%Y%m%d_%H%M%S')
+#		echo "$T count stars"
+#		S=$(cat ${LIVEDIR}/${CAMNAME}_temp2.jpg|jpegtopnm 2>/dev/null|ppmtopgm|convert -crop 1000x1000+796+472 - - |convert - crop.fits && image2xy -O -g 9 crop-0.fits|awk '{ print $3 }')
+#		echo "stars=[$S]"
+#	echo "update ${CAMNAME}camstars.rrd -t stars N:$S > /dev/shm/rrdupdate_${CAMNAME}camstars"
+#	echo "update ${CAMNAME}camstars.rrd -t stars N:$S" > /dev/shm/rrdupdate_${CAMNAME}camstars
+#	else
+#		echo "$T it is too bright, do not bother counting stars"
+#		S=0
+#	fi
 
+	if [[ $RED -eq 0 && $GREEN -gt 200 && $BLUE -eq 0 ]]; then
+        echo "ANOMALY correct, set to min 100"
+        SHUTTERSPEED=100
+        MUSTGOFAST=1
+    else
+        MUSTGOFAST=0
+    fi
+
+	T=$(date '+%Y%m%d_%H%M%S')
     NOW=$(date '+%s')
     DIFF=$((NOW - START))
     if [[ $DIFF -ge $TARGETPERIOD ]]; then
 		LATE=$((DIFF - TARGETPERIOD))
-        echo "We're $LATE s late to meet the $TARGETPERIOD s period, sleep 10 anyway"
+        echo "$T We're $LATE s late to meet the $TARGETPERIOD s period, sleep 10 anyway"
 		SLEEP=10
     else
         MUSTSLEEP=$((TARGETPERIOD - DIFF))
-        echo "Sleep $MUSTSLEEP s to match the $TARGETPERIOD s period"
+        echo "$T Sleep $MUSTSLEEP s to match the $TARGETPERIOD s period"
 		SLEEP=$MUSTSLEEP
     fi
 
 	OLDEXPOSURE=$(/bin/echo "scale=6; $OLDSHUTTERSPEED/1000000" | /usr/bin/bc -l)
 	NEWEXPOSURE=$(/bin/echo "scale=6; $SHUTTERSPEED/1000000" | /usr/bin/bc -l)
-	LINE="hazcam2 rpi2\n$(date -u '+%Y-%m-%dT%H:%M:%SZ')\nAverage pixel value = $AVERAGEVALUE\nExposure $OLDEXPOSURE s\n (next $NEWEXPOSURE)\nISO=$ISO\n (next $ISO)\nSleep $SLEEP s"
-	echo "convert /mnt/live/hazcam2_temp2.jpg -fill white -pointsize 60 -annotate +50+90 "$LINE" /mnt/live/hazcam2_temp3.jpg"
-	convert /mnt/live/hazcam2_temp2.jpg -fill white -pointsize 60 -annotate +50+90 "$LINE" /mnt/live/hazcam2_temp3.jpg
+	LINE="${CAMNAME} ${CAMHOST}\n$(date -u '+%Y-%m-%dT%H:%M:%SZ')\nRGB $RED $GREEN $BLUE ($AVERAGEVALUE)\nExposure $OLDEXPOSURE s\n (next $NEWEXPOSURE)\nISO $ISO\n (next $ISO)\nSleep $SLEEP s"
+    COMMAND="convert ${FILENAMEDATE}-${CAMHOST}.jpg -fill white -pointsize 60 -annotate +50+90 \"$LINE\" ${LIVEDIR}/${CAMNAME}_temp3.jpg"
+    T=$(date '+%Y%m%d_%H%M%S') ; echo "$T $COMMAND" #; $COMMAND
+    convert ${FILENAMEDATE}-${CAMHOST}.jpg -fill white -pointsize 60 -annotate +50+90 "$LINE" ${LIVEDIR}/${CAMNAME}_temp3.jpg
 
-	echo "mv /mnt/live/hazcam2_temp3.jpg /mnt/live/hazcam2.jpg"
-	mv /mnt/live/hazcam2_temp3.jpg /mnt/live/hazcam2.jpg
+    COMMAND="mv ${LIVEDIR}/${CAMNAME}_temp3.jpg ${LIVEDIR}/${CAMNAME}.jpg"
+    T=$(date '+%Y%m%d_%H%M%S') ; echo "$T $COMMAND" ; $COMMAND
 
+	/usr/bin/logger -t ${0##*/} -i "Exposure $OLDEXPOSURE s, Average $AVERAGEVALUE, Factor $DELTAFACTOR new exposure $NEWEXPOSURE s"
+
+	T=$(date '+%Y%m%d_%H%M%S')
 	if [[ $MUSTGOFAST -eq 1 ]]; then
-        echo "do not sleep $SLEEP because MUSTGOFAST is set"
+		echo "$T MUSTGOFAST, skip sleep $SLEEP"
 		continue
+	else
+		echo "$T sleep $SLEEP"
+		sleep $SLEEP
 	fi
-
-	echo "sleep $SLEEP"
-	sleep $SLEEP
 
 done
 
